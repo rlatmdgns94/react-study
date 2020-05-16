1312abf7b5ba444cbf24e638d70e3d05d7cd7918
kimseunghun <rlatmdgns94@naver.com>
2020-05-17 00:26:13 +0900

    apply material

---
+++ management/src/components/Customer.js
@@ -0,0 +1,21 @@
+import React from "react"; // 리액트 불러오기
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+class Customer extends React.Component {
+  render() {
+    return (
+      <TableRow>
+        <TableCell>{this.props.id}</TableCell>
+        <TableCell>
+          <img src={this.props.image} alt="profile" />
+        </TableCell>
+        <TableCell>{this.props.name}</TableCell>
+        <TableCell>{this.props.birthday}</TableCell>
+        <TableCell>{this.props.gender}</TableCell>
+        <TableCell>{this.props.job}</TableCell>
+      </TableRow>
+    );
+  } //그려지는 내용
+} // 리액트의 컴포넌트 형태로 class 정의
+
+export default Customer; //Customer 내보내기
